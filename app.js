891714d0e80193392a15b5d9a86a676ffeb61f14
André Gonçalves <andgcv@gmail.com>
2019-10-30 11:30:50 +0000

    Forecast function and file, updated output

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-const geocode = require ('./utils/geocode.js')
-
-// const url = 'https://api.darksky.net/forecast/***REMOVED***/37.8267,-122.4233'
-
-// request({url: url, json: true}, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to weather service!')
-//     } else if (response.body.error) {
-//         console.log('Unable to find location')
-//     } else {
-//         console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees fahrenheit.')
-//     }
-// })
-
-
+const geocode = require('./utils/geocode.js')
+const forecast = require('./utils/forecast.js')
+ 
 // On the callback function, we either get an error, or we get a response with data (this can be the response we want or an error response)
-geocode('Lisbon', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+geocode('Belas Portugal', (error, geoData) => {
+    if (error) {
+        return console.log('Error:', error)
+    }
+    // Callback chaining
+    forecast(geoData.longitude, geoData.latitude, (error, forecastData) => {
+        if (error) {
+            return console.log('Error:', error)
+        }
+        console.log(geoData.location)
+        console.log(forecastData)
+    })
 })
