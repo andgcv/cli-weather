28222779b791344299de0c97437f0adc1c007264
André Gonçalves <andgcv@gmail.com>
2019-10-30 11:30:50 +0000

    Forecast function and file, updated output

---
+++ utils/forecast.js
@@ -0,0 +1,24 @@
+const request = require('request')
+
+const forecast = (longitude, latitude, callback) => {
+    url = `https://api.darksky.net/forecast/658a65f9b897b3be4678fa10003712ac/${latitude},${longitude}?units=si`
+
+    request({url: url, json: true}, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (response.body.error) {
+            callback(response.body.error, undefined)
+        } else {
+            callback(undefined, `${response.body.daily.data[0].summary}\nCurrent temperature: ${response.body.currently.temperature} degrees Celsius.\nMinimum temperature: ${response.body.daily.data[0].temperatureMin} degrees Celsius.\nMaximum temperature: ${response.body.daily.data[0].temperatureMax} degrees Celsius.\nChance of precipitation: ${response.body.currently.precipProbability * 100.0}%.`)
+                // {
+                //     summary: response.body.daily.data[0].summary,
+                //     temperature: response.body.currently.temperature,
+                //     minTemperature: response.body.daily.data[0].temperatureMin,
+                //     maxTemperature: response.body.daily.data[0].temperatureMax,
+                //     precipProbability: response.body.currently.precipProbability
+                // }
+        }
+    })
+}
+
+module.exports = forecast
