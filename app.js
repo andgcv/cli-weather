134f0f4d746aa81222d423887ef04fde69f285ca
André Gonçalves <andgcv@gmail.com>
2019-10-30 14:44:42 +0000

    Location from user input, documentation on functions

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
- 
-// On the callback function, we either get an error, or we get a response with data (this can be the response we want or an error response)
-geocode('Belas Portugal', (error, geoData) => {
+const process = require('process')
+
+const address = process.argv[2]
+
+/*
+Take in the address that the user provided
+    If the user didn't provide an address, request one
+    If there's a low-level error, log it
+    If the response returns an error, log it as well
+    Else, when all goes well:
+        Gather the longitude, latitude and location with Mapbox's Geocoding API
+        Use longitude and latitude to gather the forecast from the DarkSky's API
+        Display this information to the user
+*/
+geocode(address, (error, geoData) => {
+    if (!address) {
+        return console.log('Please provide a location.')
+    }
     if (error) {
         return console.log('Error:', error)
     }
